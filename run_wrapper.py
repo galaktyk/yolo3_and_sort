3857ffa0e4f60237d87e77ecd04fcc0a3016858a
Unknown <galaktyk01@gmail.com>
2018-10-21 20:11:03 +0700

    show detect color

--- run_wrapper.py
+++ run_wrapper.py
@@ -31,18 +31,18 @@ def main(yolo):
 
 
  
-    source='MOT_1.mp4'  # 0 for webcam or youtube or jpg
+    source='Walking Next to People.mp4'  # 0 for webcam or youtube or jpg
     FLAGScsv=0
 
     if FLAGScsv :
         csv_obj=save_csv() 
         
- 
+    colors = {"male":(0,0,255),"female":(255,0,0)}
         
     
     tpro=0.
    # Definition of the parameters
-    max_cosine_distance = 0.7
+    max_cosine_distance = 1.5
     nn_budget = None
     nms_max_overlap = 1.0
     
@@ -51,15 +51,17 @@ def main(yolo):
     encoder = gdet.create_box_encoder(model_filename,batch_size=1)
     
     metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-    tracker = Tracker(metric)
+    tracker = Tracker(metric,max_iou_distance=0.7, max_age=30, n_init=1)
 
 
     video_capture = cv2.VideoCapture(source)           
        
 
     print('video source : ',source)   
-    
-    #out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (608,608))
+  
+    out = cv2.VideoWriter() 
+    out.open('output.mp4',cv2.VideoWriter_fourcc(*'mpeg'),25,(1280,720),True)
+
 #  ___________________________________________________________________________________________________________________________________________MAIN LOOP
     while True:
 
@@ -73,81 +75,82 @@ def main(yolo):
                 frame = numpy.array(Image.open(buff), dtype=numpy.uint8) #RGB
         
                 
-                frame = cv2.resize(frame,(416,416))
+                
             except OSError :
                 continue
             except TypeError:
                 continue
 
         else :
-            ret, frame = video_capture.read()          
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)                  
+            ret, frame = video_capture.read()       
             if ret != True:
-                break;
+                print('Stream End')
+                break;   
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)                  
+
                 
 
         # ______________________________________________________________________________________________________________________________DETECT WITH YOLO 
         t1 = time.time()       
 
-        boxs = yolo.detect_image(frame,boxes_only = True)
+        return_boxes,return_classes = yolo.detect_image(frame,boxes_only = True)
 
-        print(boxs)
+        print(return_classes)
 
-        features = encoder(frame,boxs)       
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) #change to BGR for show 
+        features = encoder(frame,return_boxes)       
+       
 
 
         # score to 1.0 here).
-        detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(boxs, features)]     
+        detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(return_boxes, features)]     
 
-        # Run non-max suppression.
-        boxes = np.array([d.tlwh for d in detections])
-        scores = np.array([d.confidence for d in detections])
-        indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores) #index that filtered
+        # # Run non-max suppression.
+        # boxes = np.array([d.tlwh for d in detections])
+        # scores = np.array([d.confidence for d in detections])
+        # indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores) #index that filtered
         
-        detections = [detections[i] for i in indices]
+        # detections = [detections[i] for i in indices]
         
         # ______________________________________________________________________________________________________________________________DRAW DETECT BOX
 
-        for det in detections:
-            bbox = det.to_tlbr()
-            cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(0,0,255), 1)
+        for i in range(0,len(detections)):
+            bbox = detections[i].to_tlbr()
+            cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),colors[return_classes[i]], 2)
 
 
 
-       #  # ___________________________________________________________________________Call the tracker 
-       #  tracker.predict()
-       #  tracker.update(detections)
+        # ___________________________________________________________________________Call the tracker 
+        tracker.predict()
+        tracker.update(detections)
         
         
-
        
 
-       # # __________________________________________________________________________________________________________________________DRAW TRACK RECTANGLE      
-       #  ina_now=set();inb_now=set()   
-       #  for track in tracker.tracks:
-       #      if track.is_confirmed() and track.time_since_update >1 :
-       #          continue 
+       # __________________________________________________________________________________________________________________________DRAW TRACK RECTANGLE      
+        ina_now=set();inb_now=set()   
+        for track in tracker.tracks:
+            if track.is_confirmed() and track.time_since_update >1 :
+                continue 
             
-       #      bbox = track.to_tlbr()
+            bbox = track.to_tlbr()
             
 
-       #      cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
-       #      cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])+30),cv2.FONT_HERSHEY_SIMPLEX, 5e-3 * 200, (0,255,0),3)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 1)
+            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])+30),cv2.FONT_HERSHEY_SIMPLEX, 5e-3 * 200, (0,255,0),3)
 
-       #      dot=int(int(bbox[0])+((int(bbox[2])-int(bbox[0]))/2)),int(bbox[3]-15)
-            
-       #      cv2.circle(frame,dot, 10, (0,0,255), -1)
-
+           
                  
 
 
            
 
                 
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) #change to BGR for show 
 
+        out.write(frame)
         cv2.imshow('', frame)
         
+        print(frame.shape[:2])
         print('process time : ',time.time()-tpro)
         tpro=time.time()
 
@@ -155,9 +158,9 @@ def main(yolo):
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+    out.release()
     video_capture.release()
-    #out.release()
+    
     cv2.destroyAllWindows()
 
 
